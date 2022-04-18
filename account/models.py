962a24ed44950d5f223340b5523dd076ac4751a2
Sixto <sixtoh@sixtoh.pc>
2022-04-18 16:41:24 +0200

    Añadido script generarbbdd y solucionado models.py

--- account/models.py
+++ account/models.py
@@ -1,8 +1,106 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
+largeText = 128
+smallText = 64
 
-class MyUser(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    DNI = models.CharField(max_length = 9)
+class TiposDeGamas(models.TextChoices):
+     ALTA = 'Alta'
+     MEDIA = 'Media'
+     BAJA = 'Baja'
+     
+class TiposDeUsuarios(models.TextChoices):
+     NEGOCIO = 'Negocio'
+     PARTICULAR = 'Particular'
+     
+class TiposDeEstados(models.TextChoices):
+     LIBRE = 'Libre'
+     RESERVADO = 'Reservado'
+     NO_DISPONIBLE = 'No disponible'
+
+class Usuario(models.Model):
+     dni = models.CharField(max_length = smallText, primary_key = True)
+     nombre = models.CharField(max_length = largeText)
+     apellidos = models.CharField(max_length = largeText)
+     correo = models.CharField(max_length = largeText)
+     contraseña = models.CharField(max_length = smallText)
+     tipo = models.CharField(max_length = smallText, choices = TiposDeUsuarios.choices)
+     def __str__(self):
+        return self.dni
+     
+class Tarifas(models.Model):
+     tipo = models.IntegerField()
+     gama = models.CharField(max_length = smallText, choices = TiposDeGamas.choices, default = 'No Disponible')
+     def __str__(self):
+        return 'Tipo ' + self.tipo + ' Gama ' +self.gama 
+     
+class Oficina(models.Model):
+     ciudad = models.CharField(max_length = largeText)
+     facturado = models.DecimalField(default = 0.0, max_digits=19, decimal_places=2)
+     def __str__(self):
+        return self.ciudad
+     
+class Descuentos(models.Model):
+     porcentaje = models.DecimalField(max_digits=3, decimal_places=2)
+     def __str__(self):
+        return self.porcentaje
+     
+class Extras(models.Model):
+     extra = models.CharField(max_length = largeText)
+     def __str__(self):
+        return self.extra
+     
+class Coches(models.Model):
+     id_oficina = models.ForeignKey(
+          Oficina,
+          on_delete=models.CASCADE
+     )
+     marca = models.CharField(max_length = largeText)
+     modelo = models.CharField(max_length = largeText)
+     gama = models.CharField(max_length = smallText, choices = TiposDeGamas.choices)
+     estado = models.CharField(max_length = smallText, choices = TiposDeEstados.choices)
+     def __str__(self):
+        return self.marca + ' ' + self.modelo
+     
+class Opciones(models.Model):
+     id_coche = models.ForeignKey(
+           Coches,
+           on_delete = models.CASCADE
+     )
+     opcion = models.CharField(max_length = largeText)
+     def __str__(self):
+        return self.id_coche + ' opcion: ' + self.opcion
+      
+class Reserva(models.Model):
+     id_usuario = models.ForeignKey(
+          Usuario,
+          on_delete = models.CASCADE
+     )
+     id_oficina_rec = models.ForeignKey(
+          Oficina,
+          on_delete = models.CASCADE,
+          related_name = 'lugar_recogida'
+     )
+     id_oficina_dev = models.ForeignKey(
+          Oficina,
+          on_delete = models.CASCADE,
+          related_name = 'lugar_devolucion'
+     )
+     id_coche = models.ForeignKey(
+          Coches,
+          on_delete = models.CASCADE
+     )
+     id_tarifa = models.ForeignKey(
+          Tarifas,
+          on_delete = models.CASCADE
+     )
+     id_extra = models.ManyToManyField(Extras)
+     id_opciones = models.ManyToManyField(Opciones)
+     fecha_rec = models.DateField()
+     fecha_dev = models.DateField()
+     hora_rec = models.TimeField()
+     hora_dev = models.TimeField()
+     tarjeta_credito = models.IntegerField()
+     def __str__(self):
+        return 'Coche ' + self.id_coche
+     
+      
---
+++ generarbbdd.py
@@ -0,0 +1,88 @@
+from account.models import *
+
+"""
+class TiposDeGamas(models.TextChoices):
+     ALTA = 'Alta'
+     MEDIA = 'Media'
+     BAJA = 'Baja'
+     
+class TiposDeUsuarios(models.TextChoices):
+     NEGOCIO = 'Negocio'
+     PARTICULAR = 'Particular'
+     
+class TiposDeEstados(models.TextChoices):
+     LIBRE = 'Libre'
+     RESERVADO = 'Reservado'
+     NO_DISPONIBLE = 'No disponible'
+"""
+
+#/////////////////USUARIO/////////////////
+
+#Generar dato
+user = Usuario(dni="83140123X", nombre="Nombre", apellidos="Apellidos", correo="correofalso@yopmail.com", contraseña="Pass_word42", tipo=TiposDeUsuarios.PARTICULAR)
+user.save()
+
+
+#/////////////////TARIFAS/////////////////
+
+#Generar dato
+t1 = Tarifas(tipo=1, gama=TiposDeGamas.MEDIA)
+t1.save()
+
+
+#/////////////////OFICINA/////////////////
+
+#Generar dato sin facturado
+of1 = Oficina(ciudad="Nombre ciudad")
+of1.save()
+
+#Generar dato con facturado dado
+of2 = Oficina(ciudad="Nombre ciudad", facturado=262.1)
+of2.save()
+
+#/////////////////DESCUENTOS/////////////////
+
+#Generar dato
+Descuentos(porcentaje = 0.15).save()
+
+
+#/////////////////EXTRAS/////////////////
+
+#Generar dato
+ex1 = Extras(extra="Nombre del extra")
+ex1.save()
+
+
+#/////////////////COCHES/////////////////
+
+#Generar dato
+Coches(id_oficina=of1, marca="Marca del coche", modelo="Modelo del coche", gama=TiposDeGamas.ALTA, estado=TiposDeEstados.NO_DISPONIBLE).save()
+
+c1 = Coches(id_oficina=of2, marca="Marca del coche 2", modelo="Modelo del coche 2", gama=TiposDeGamas.BAJA, estado=TiposDeEstados.LIBRE)
+c1.save()
+
+
+#/////////////////OPCIONES/////////////////
+
+#Generar dato
+opc1 = Opciones(id_coche=c1, opcion="Opcion disponible para el coche")
+opc1.save()
+
+
+#/////////////////RESERVA/////////////////
+
+#Generar dato
+r1 = Reserva(id_usuario=user, id_oficina_rec=of1, id_oficina_dev=of1, id_coche=c1, id_tarifa=t1, fecha_rec='2020-05-01', fecha_dev='2020-05-26', hora_rec='15:00:00', hora_dev='9:30:00', tarjeta_credito=1234567890123456)
+r1.save()
+
+#Añadir extra
+r1.id_extra.add(ex1)
+
+#Repetir no añade duplicados
+r1.id_extra.add(ex1)
+
+#Añadir opciones
+r1.id_opciones.add(opc1)
+
+#Repetir no añade duplicados
+r1.id_opciones.add(opc1)
