5da0051a32260700a07364e469bfef67ec4c31f0
lbm364dl <geometrydashm364@gmail.com>
2022-04-18 19:35:47 +0200

    removed registration app, use account instead

--- registration/form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MyUserForm(forms.Form):
-    name = forms.CharField(label = 'Your name', max_length = 20)
-    mail = forms.CharField(label = 'Your email', max_length = 20)
-    password = forms.CharField(label = 'Your password', max_length = 20)
--- registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'registration'
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('jaja/', views.jaja, name='jaja'),
-]
