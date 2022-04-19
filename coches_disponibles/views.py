b9543128f47f5c43f972b338db3bd8ef94534c11
Bruce <bruce_leon1@hotmail.com>
2022-04-19 14:34:44 +0200

    listacochesfull

--- coches_disponibles/views.py
+++ coches_disponibles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Coches
 
 # Create your views here.
 
 def coches_disponibles(request):
-    return render(request, 'home/cochesdisponibles.html')
+    coches = Coches.objects.all()
+    return render(request, 'home/cochesdisponibles.html',{'coches': coches})
     
 #render(request, 'recogida.html')
