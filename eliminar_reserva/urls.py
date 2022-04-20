199a3dbe9db98b66787ac40e95027753b7651141
treefngrs <jfuertesllorens@gmail.com>
2022-04-20 23:16:44 +0200

    eliminar-reserva

--- eliminar_reserva/urls.py
+++ eliminar_reserva/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'reservas_'
 urlpatterns = [
-   
-    path('', views.eliminar_reserva),
+    path('', views.ver_reservas),
+    path('eliminar_reserva/<int:id_reserva>', views.eliminar_reserva, name = 'eliminar_reserva'),
 ]
