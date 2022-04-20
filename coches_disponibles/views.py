dd27dd609e2901056761e1068d353bcb3876f014
lbm364dl <geometrydashm364@gmail.com>
2022-04-20 17:21:01 +0200

    show coches libres

--- coches_disponibles/views.py
+++ coches_disponibles/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Coches
+from .models import Coches, TiposDeEstados
 
 # Create your views here.
 
 def coches_disponibles(request):
-    coches = Coches.objects.all()
+    coches = Coches.objects.filter(estado = TiposDeEstados.LIBRE)
     return render(request, 'home/cochesdisponibles.html',{'coches': coches})
     
-#render(request, 'recogida.html')
