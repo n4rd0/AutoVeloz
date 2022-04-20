714383f2f8733dc519978f27f4a3e1d9f87d69fb
lbm364dl <geometrydashm364@gmail.com>
2022-04-20 12:57:56 +0200

    improved reserva

--- recogida_entrega/views.py
+++ recogida_entrega/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from . import forms
+from account.models import Usuario
+from tarifas_disponibles.models import Tarifas
+from coches_disponibles.models import Coches
+from modificar_reserva.models import Reserva
+import datetime
+
+def get_temporada(fecha_rec):
+    month = fecha_rec.month
+    if 1 <= month <= 5:
+        return 1
+    elif 6 <= month <= 9:
+        return 2
+    else:
+        return 3
 
 # Create your views here.
 
@@ -8,5 +22,34 @@ def recogida_entrega(request, id_coche, id_tarifa):
     if request.method == 'GET':
         form = forms.crearReserva()
         return render(request, 'home/recogida.html', {'form': form})
-    
-#render(request, 'recogida.html')
+    else:
+        post = request.POST
+        form = forms.crearReserva(post)
+        tarifa = Tarifas.objects.get(id = id_tarifa)
+
+        if form.is_valid():
+            dat = form.cleaned_data
+            rec = datetime.datetime.combine(dat['fecha_rec'], dat['hora_rec'])
+            dev = datetime.datetime.combine(dat['fecha_dev'], dat['hora_dev'])
+            # ver mensajes de error personalizados en forms.py
+            if get_temporada(dat['fecha_rec']) != tarifa.temporada:
+                dat['fecha_rec'] = ''
+                form = forms.crearReserva(dat)
+                return render(request, 'home/recogida.html', {'form' : form})
+            elif dev <= rec:
+                dat['fecha_rec'] = 'invalid'
+                form = forms.crearReserva(dat)
+                return render(request, 'home/recogida.html', {'form' : form})
+            else:
+                coche = Coches.objects.get(id = id_coche)
+                Reserva.objects.create(
+                        **dat, 
+                        coche = coche,
+                        oficina_rec = coche.oficina,
+                        tarifa = Tarifas.objects.get(id = id_tarifa),
+                        usuario = Usuario.objects.get(dni = request.user.username),
+                    )
+                return HttpResponseRedirect('/modificar_reserva/')
+        else:
+            form = forms.crearReserva()
+            return render(request, 'home/recogida.html', {'form' : form})
