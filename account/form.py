f304e45bdee07e4c5abb4611d912e3ee64cceee9
lbm364dl <geometrydashm364@gmail.com>
2022-04-20 17:06:58 +0200

    error messages

--- account/form.py
+++ account/form.py
@@ -5,10 +5,20 @@ largeText = 128
 smallText = 64
 
 class MyUserForm(forms.Form):
-    username = forms.CharField(label = 'DNI', max_length = 9)
+    username = forms.CharField(label = 'DNI', max_length = 9, error_messages = {'required' : 'Formato incorrecto'})
     first_name = forms.CharField(label = 'Nombre', max_length = largeText)
     last_name = forms.CharField(label = 'Apellidos', max_length = largeText)
-    email = forms.CharField(label = 'Correo', max_length = largeText)
-    password = forms.CharField(widget = forms.PasswordInput, label = 'Contraseña', max_length = smallText)
-    password_re = forms.CharField(widget = forms.PasswordInput, label = 'Repetir contraseña', max_length = smallText)
+    email = forms.CharField(label = 'Correo', max_length = largeText, error_messages = {'required' : 'Formato incorrecto'})
+    password = forms.CharField(
+            widget = forms.PasswordInput, 
+            label = 'Contraseña', 
+            max_length = smallText, 
+            error_messages = {'required' : 'Las contraseñas no coinciden'}
+        )
+    password_re = forms.CharField(
+            widget = forms.PasswordInput, 
+            label = 'Repetir contraseña', 
+            max_length = smallText, 
+            error_messages = {'required' : 'Las contraseñas no coinciden'}
+        )
     user_type = forms.ChoiceField(label = 'Tipo usuario', choices = TiposDeUsuarios.choices)
