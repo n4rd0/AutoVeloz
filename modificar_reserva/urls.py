a8413b2db9357dd2f4df6afbdff9927aad710a8c
lbm364dl <geometrydashm364@gmail.com>
2022-04-20 13:33:49 +0200

    change names

--- modificar_reserva/urls.py
+++ modificar_reserva/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'reservas'
 urlpatterns = [
-   
-    path('', views.modificar_reserva),
+    path('', views.ver_reservas),
+    path('modificar_reserva/<int:id_reserva>', views.modificar_reserva, name = 'modificar_reserva'),
 ]
--- modificar_reserva/views.py
+++ modificar_reserva/views.py
@@ -4,7 +4,9 @@ from modificar_reserva.models import Reserva
 
 # Create your views here.
 
-def modificar_reserva(request):
+def ver_reservas(request):
     reservas = Reserva.objects.all()
-    return render(request, 'home/modificar_reserva.html', {'reservas' : reservas})
-    
+    return render(request, 'home/ver_reservas.html', {'reservas' : reservas})
+
+def modificar_reserva(request, id_reserva):
+    return HttpResponse('jajaxd')
