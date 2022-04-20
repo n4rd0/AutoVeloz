18dc20379e5eca448b86cbb353627c24a3b6eae3
lbm364dl <geometrydashm364@gmail.com>
2022-04-20 17:21:29 +0200

    show only reservas of user

--- modificar_reserva/views.py
+++ modificar_reserva/views.py
@@ -5,7 +5,7 @@ from modificar_reserva.models import Reserva
 # Create your views here.
 
 def ver_reservas(request):
-    reservas = Reserva.objects.all()
+    reservas = Reserva.objects.filter(usuario__dni = request.user.username)
     return render(request, 'home/ver_reservas.html', {'reservas' : reservas})
 
 def modificar_reserva(request, id_reserva):
