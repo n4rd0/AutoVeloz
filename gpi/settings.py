d7a9424a7b7c983f4b8f18768ae084ad0ab5f337
Sixto <sixtoh@sixtoh.pc>
2022-05-06 13:05:57 +0200

    añadidos templates

--- gpi/settings.py
+++ gpi/settings.py
@@ -127,6 +127,10 @@ TIME_ZONE = 'Europe/Madrid'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "templates/home/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
